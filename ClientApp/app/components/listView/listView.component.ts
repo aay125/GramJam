91573b7f4af0062a64584bba6e12c3eacc908aa1
aay125 <alashkari1995@gmail.com>
2018-07-12 20:03:18 -0500

    list view for employee

---
+++ ClientApp/app/components/listView/listView.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
+import { Router } from '@angular/router';
+
+const items = [
+    {
+        name: "Something"
+    },
+    {
+        name: "Something"
+    },
+    {
+        name: "Something"
+    },
+    {
+        name: "Something"
+    }
+]
+
+@Component({
+    selector: 'listView',
+    templateUrl: './listView.component.html',
+    styleUrls: ['./listView.component.css']
+})
+
+export class ListViewComponent implements OnInit {
+
+    items = items;
+
+    constructor() { }
+
+    ngOnInit() {
+        
+    }
+}
