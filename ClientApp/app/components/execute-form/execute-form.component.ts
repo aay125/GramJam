a28585f9ae70eb31355ae18f241637609e6e7386
Miguel Perez <perez.miguel1321@gmail.com>
2018-07-24 19:25:10 -0500

    Executive form and css position changes to login.

--- ClientApp/app/components/execute-form/execute-form.component.ts
+++ ClientApp/app/components/execute-form/execute-form.component.ts
@@ -1,4 +1,5 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
     selector: 'app-execute-form',
@@ -6,9 +7,55 @@
     styleUrls: ['./execute-form.component.css']
 })
 /** execute-form component*/
-export class ExecuteFormComponent {
+export class ExecuteFormComponent implements OnInit{
     /** execute-form ctor */
-    constructor() {
+    executiveForm: FormGroup;
+    get section2(): FormArray{
+        return <FormArray>this.executiveForm.get('section2');
+    }
+    get section5(): FormArray {
+        return <FormArray>this.executiveForm.get('section5');
+    }
 
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.executiveForm = this.fb.group({
+            section2: this.fb.array([this.buildEntry2()]),
+            section5: this.fb.array([this.buildEntry5()])
+        });
+    }
+    addEntry2(): void {
+        this.section2.push(this.buildEntry2());
+    }
+    addEntry5(): void {
+        this.section5.push(this.buildEntry5());
+    }
+    buildEntry2(): FormGroup {
+        return this.fb.group({
+            percent: ['', [Validators.required, Validators.minLength(3)]],
+            rating: ['', [Validators.required, Validators.minLength(3)]],
+            responsibility: ['', [Validators.required, Validators.minLength(3)]],
+            summary: ['', [Validators.required, Validators.minLength(3)]]
+        });
+    }
+    buildEntry5(): FormGroup {
+        return this.fb.group({
+            time: ['', [Validators.required, Validators.minLength(3)]],
+            summary: ['', [Validators.required, Validators.minLength(3)]]
+        });
+    }
+    populateTestData(): void {
+        this.executiveForm.patchValue({
+            percent: '100%',
+            rating: '11/10',
+            responsibility: 'Make the code gud.',
+            summary: 'This is a summary.'
+        });
+    }
+
+    save() {
+        console.log(this.executiveForm);
+        console.log('Saved: ' + JSON.stringify(this.executiveForm.value));
     }
 }
